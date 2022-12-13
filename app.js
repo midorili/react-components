19c77b50700fc21d9e366d98ea4d577fb54b5caa
Midori <midorili998@gmail.com>
2022-12-12 19:54:47 -0800

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// TODO
+const GroceryList = () => (
+  <div>
+  <GroceryListItem grocery={['eggs', 'milk', 'butter']}/>
+  </div>
+)
+
+// const Eggs = () => (
+//   <ul>
+//     <li>eggs</li>
+//   </ul>
+
+// )
+
+// const Milk = () => (
+//   <ul>
+//     <li>milk</li>
+//   </ul>
+// )
+
+// const GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.grocery[0]}</li>
+//     <li>{props.grocery[1]}</li>
+//     <li>{props.grocery[2]}</li>
+//   </ul>
+// )
+const { useState } = React;
+
+const GroceryListItems = (props) => {
+  <li>{props.grocery}</li>
+  const[isDone, setIsDone] = useState(false);
+
+
+  const style = {
+    fontWeight: isDone ? 'bold' : 'none'
+  };
+
+  return  (
+    <li style={style} onMouseEnter={() => setIsDone(!isDone)}>
+      {props.grocery}
+    </li>
+
+
+  );
+}
+const GroceryListItem = (props) => (
+  <ul>
+     {props.grocery.map((item) => (
+    <GroceryListItems grocery={item} />
+  ))}
+  </ul>
+)
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
